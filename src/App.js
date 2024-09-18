6790d13d2ca568b2474898a6e512ebc68f5b24e6
Hadiquaa <hadiqua350@gmail.com>
2024-09-19 03:48:21 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [userData, setUserData] = useState([]);
+  const [filterData, setFilterData] = useState(userData);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('https://66e6e80b17055714e58ae319.mockapi.io/users');
+      const data = await response.json();
+      setUserData(data);
+    };
+    fetchData();
+  }, []);
+
+  let handleClick = (event) => {
+
+    let search = event.target.value;
+    if (search) {
+      let filter = userData.filter(user => user.name.toLowerCase().startsWith(search.toLowerCase()));
+      console.log(filter);
+      setFilterData(filter)
+    }
+    else
+      setFilterData([]);
+    console.log(filterData);
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Search Users</h2>
+      <input type='text' id='myInput' placeholder='Search for names..' onKeyUp={handleClick}></input>
+      <ul>
+        {filterData.map((user) => {
+          return <li key={user.id}><a href='#'>{user.name}</a></li>
+        })}
+      </ul>
     </div>
   );
 }
